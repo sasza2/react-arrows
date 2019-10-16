52a908226fb40379fb2d6ec2758d9abc5dac4a03
sasza <sasza@onet.eu>
2019-10-16 22:26:48 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const Arrow = () => null
+
+export default Arrow
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
