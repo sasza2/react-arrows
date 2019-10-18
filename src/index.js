adc65151579299d19d684309b77244f4ea20c82f
sasza <sasza@onet.eu>
2019-10-18 23:35:15 +0200

    observer hook

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
-import React, { useLayoutEffect, useRef } from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 import arrowCreate, { DIRECTION } from 'arrows-svg'
+
+const useObserver = (props) => {
+  const [mounted, setMounted] = useState(false)
+
+  useLayoutEffect(() => {
+    const observe = () => {
+      const from = props.node.from()
+      const to = props.node.to()
+      if(from && to){
+        setMounted(true)
+        return true
+      }
+    }
+
+    if (observe()) return
+
+    const timer = setInterval(observe, 150)
+    return () => clearInterval(timer)
+  }, [props])
+
+  return mounted
+}
 
 const Arrow = (props) => {
   const arrowRef = useRef()
+  const mounted = useObserver(props)
 
   useLayoutEffect(() => {
-    const arrow = arrowCreate(props)
+    if (!mounted) return
+    
+    let arrow
+    try {
+      arrow = arrowCreate(props)
+    } catch(e){
+      console.warn(e);
+      return;
+    }
     if (arrowRef.current) arrowRef.current.appendChild(arrow.node)
 
     return () => {
       clearInterval(arrow.timer)
       if (arrowRef.current) arrowRef.current.removeChild(arrow.node)
     }
-  }, [props])
+  }, [mounted, props])
 
   return (
     <span ref={arrowRef} />
